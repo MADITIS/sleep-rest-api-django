28f40c412dcbb44f020c8e3d2a2342fa70cc4e7d
Manik Das <97931292+MADITIS@users.noreply.github.com>
2023-05-15 11:11:29 +0530

    Update urls.py

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import register
+from .views import register, home
 
 urlpatterns = [
     path('create/', register, name='register'),
-]
+    path('<int:userId>/improvement/', home, name='not implemented')
+]path
